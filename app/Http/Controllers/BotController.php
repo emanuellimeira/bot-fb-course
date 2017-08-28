30eb8bc1b574474e8e4185f098d62b32fe70fbf7
Emanuel Limeira <emanuellimeira@gmail.com>
2017-08-28 13:24:24 -0300

    testes com arquivos de midia

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use CodeBot\WebHook;
+use CodeBot\Message\File;
 use CodeBot\Message\Text;
+use CodeBot\Message\Audio;
+use CodeBot\Message\Video;
 use CodeBot\SenderRequest;
 use Illuminate\Http\Request;
 
@@ -27,7 +30,19 @@ class BotController extends Controller
     	$text = new Text($senderId);
     	$callSendApi = new CallSendApi(config('botfb.pageAccessToken'));
     	$callSendApi->make($text->message('Oii, eu sou um bot...'));
-    	$callSendApi->make($text->message('Você digitou: '. $message));
+        $callSendApi->make($text->message('Você digitou: '. $message));
+
+        $message = new Image($senderId);
+        $callSendApi->make($message->message('https://calm-mountain-45407.herokuapp.com/img/homer.gif'));
+
+        $message = new Audio($senderId);
+        $callSendApi->make($message->message('https://calm-mountain-45407.herokuapp.com/audio/woohoo.wav'));
+
+        $message = new File($senderId);
+        $callSendApi->make($message->message('https://calm-mountain-45407.herokuapp.com/file/file.zip'));
+
+        $message = new Video($senderId);
+        $callSendApi->make($message->message('https://calm-mountain-45407.herokuapp.com/video/video.mp4'));
 
     	return '';
 
